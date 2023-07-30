b4746ee5ab0f70bb602516a1d179600c54184538
Chris <118864776+darthchris623@users.noreply.github.com>
2023-07-30 12:05:50 -0400

    Update batting average-test.js
    This is the final Jasmine test code. Previous version didn't work.

--- batting average-test.js
+++ batting average-test.js
@@ -1,25 +1,44 @@
 describe('get batting average', function () {
-    beforeEach(function(){
-        const hits = document.getElementById('hits');
-        const atBats = document.getElementById('at-bats');
-
-        hits.value = 400;
-        atBats.value = 1000;
-    });
-
     it('should calculate batting average', function(){
         // arrange
-        
+        const hits = 400;
+        const atBats = 1000;
 
         // act
-        const result = battingAvg();
+        const result = battingAvg(hits, atBats);
 
         // assert
         expect(result).toEqual(0.4);
     });
 
-    afterEach(function () {
-        hits.value = '';
-        atBats.value = '';
+    it('should call appendResults() when battingAvg() is called', function () {
+        spyOn(window, "appendResults");
+
+        // Creating the element inputs and assigning their values
+        const hits = document.createElement('input');
+        const atBats = document.createElement('input');
+        hits.type = 'number';
+        atBats.type = 'number';
+        hits.value = 400;
+        atBats.value = 1000;
+
+        // Creating the submit button and adding the event listener
+        const submitButton = document.createElement('button');
+        submitButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            battingAvg(hits.value, atBats.value);
+            appendResults();
+        });
+
+        // Appending the input and button to the DOM
+        document.body.append(hits);
+        document.body.append(atBats);
+        document.body.append(submitButton);
+
+        // Triggering the "click" event on the submit button
+        submitButton.dispatchEvent(new Event('click'));
+
+        // Checking to see if appendResults() was called
+        expect(appendResults).toHaveBeenCalled();
     });
 });
